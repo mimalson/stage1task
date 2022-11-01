ff7e0b502ead482b8b36592427273d3da6bea1b7
Amina <87755052+mimalson@users.noreply.github.com>
2022-11-01 20:27:25 +0100

    Update header.js

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './header.css'
 import ME from '../images/ME.jpg'
+import MENU from '../images/.MENU/jpg'
 
 const header = () => {
   return (
     <header>
+        <div id='menu'>
+            <img className='menus' src={MENU} alt='profile DIP'/>
         <div className='header-div'>
             <img className='profile__img' src={ME} alt='profile DP'/>
             <h1 className='name'> Anjelina Jolie</h1>
